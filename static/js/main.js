45b6b2cdb499527065804ab31fe920a7f7542284
pastak <pasta0915@gmail.com>
2018-05-18 17:16:54 +0900

    URLから生成できる

--- static/js/main.js
+++ static/js/main.js
@@ -3,13 +3,45 @@
   const hairImageElement = document.querySelector('.hair');
   const filterElement = document.querySelector('#filter > feColorMatrix[type=\'hueRotate\']')
   const hueRotateContoller = document.querySelector('#hueRotateContoller');
-  if (!(filterElement && hueRotateContoller)) return
-  hueRotateContoller.addEventListener('input', () => {
+  const setHueRotate = () => {
     filterElement.setAttribute('values', hueRotateContoller.value)
-  });
+  }
+  if ((filterElement && hueRotateContoller)) {
+    hueRotateContoller.addEventListener('input', setHueRotate);
+    setHueRotate()
+  }
   const brightnessController = document.querySelector('#brightnessController')
-  if (!brightnessController) return;
-  brightnessController.addEventListener('input', () => {
+  const setBrightness = () => {
     hairImageElement.style.filter = `url(#filter) brightness(${brightnessController.value})`
+  }
+  if (brightnessController) {
+    brightnessController.addEventListener('input', setBrightness);
+    setBrightness();
+  }
+  const tweetButton = document.querySelector('#tweetButton');
+  if (tweetButton && hueRotateContoller && brightnessController) {
+    tweetButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      const message = '私のオススメpastakヘアカラーはこちら！皆さんもオススメのヘアカラーをシェアしませんか？';
+      const thisUrl = `https://haircolor.pastak.net/?hue=${hueRotateContoller.value}&brightness=${brightnessController.value}`;
+      const intentUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}&url=${encodeURIComponent(thisUrl)}&hashtags=pastak_haircolor`
+      open(intentUrl);
+    })
+  }
+  let searchQueries = {};
+  location.search.substr(1).split('&').forEach(t => {
+    const [k, v] = t.split('=');
+    searchQueries[k] = v;
   })
+  if (Object.keys(searchQueries)) {
+    history.replaceState({}, document.title, '/');
+    if (hueRotateContoller && searchQueries['hue'] !== undefined) {
+      hueRotateContoller.value = searchQueries['hue'];
+      setHueRotate();
+    }
+    if (brightnessController && searchQueries['brightness'] !== undefined) {
+      brightnessController.value = searchQueries['brightness'];
+      setBrightness();
+    }
+  }
 })();
