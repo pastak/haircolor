166ac58aac4d8130176f054ad2c9be8a94550c19
pastak <pasta0915@gmail.com>
2018-05-18 19:55:54 +0900

    canvas version

--- static/js/main.js
+++ static/js/main.js
@@ -1,50 +1,74 @@
 'use strict';
 (() => {
   if (location.protocol === 'http:' && location.hostname !== 'localhost') {
-    return location.protocol = 'https:';
+    return location.protocol = 'https:'
   }
-  const hairImageElement = document.querySelector('.hair');
-  const filterElement = document.querySelector('#filter > feColorMatrix[type=\'hueRotate\']')
-  const hueRotateContoller = document.querySelector('#hueRotateContoller');
-  const setHueRotate = () => {
-    filterElement.setAttribute('values', hueRotateContoller.value)
+
+  const hairImage = document.querySelector('#hairImage')
+  const faceImage = document.querySelector('#faceImage')
+  const canvas = document.querySelector('#resultCanvas')
+  const ctx = canvas.getContext('2d')
+
+  const updateCanvas = (filter) => {
+    ctx.drawImage(faceImage, 0, 0)
+    ctx.save()
+    ctx.filter = filter
+    ctx.globalCompositeOperation = 'overlay'
+    ctx.drawImage(hairImage, 0, 0)
+    ctx.restore()
   }
-  if ((filterElement && hueRotateContoller)) {
-    hueRotateContoller.addEventListener('input', setHueRotate);
-    setHueRotate()
-  }
+
+  const hueRotateContoller = document.querySelector('#hueRotateContoller')
   const brightnessController = document.querySelector('#brightnessController')
-  const setBrightness = () => {
-    hairImageElement.style.filter = `url(#filter) brightness(${brightnessController.value})`
+  const updateFilter = () => {
+    const filter = `hue-rotate(${hueRotateContoller.value}deg) brightness(${brightnessController.value})`
+    updateCanvas(filter)
   }
-  if (brightnessController) {
-    brightnessController.addEventListener('input', setBrightness);
-    setBrightness();
-  }
-  const tweetButton = document.querySelector('#tweetButton');
-  if (tweetButton && hueRotateContoller && brightnessController) {
-    tweetButton.addEventListener('click', (event) => {
-      event.preventDefault();
-      const message = '私のオススメpastakヘアカラーはこちら！皆さんもオススメのヘアカラーをシェアしませんか？';
-      const thisUrl = `https://haircolor.pastak.net/?hue=${hueRotateContoller.value}&brightness=${brightnessController.value}`;
-      const intentUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}&url=${encodeURIComponent(thisUrl)}&hashtags=pastak_haircolor`
-      open(intentUrl);
+  hueRotateContoller && hueRotateContoller.addEventListener('input', updateFilter)
+  brightnessController && brightnessController.addEventListener('input', updateFilter)
+
+  const saveImageButton = document.querySelector('#saveImageButton')
+
+  const images = [hairImage, faceImage]
+  const loading = images.filter(i => !i.complete)
+  if (loading.length === 0) {
+    updateFilter()
+  } else {
+    let loadedImage = 0
+    loading.forEach(i => {
+      i.addEventListener('load', () => {
+        loadedImage++
+        if (loadedImage === loading.length) updateFilter()
+      })
     })
   }
-  let searchQueries = {};
+
+
+  const tweetButton = document.querySelector('#tweetButton')
+  if (tweetButton && hueRotateContoller && brightnessController) {
+    tweetButton.addEventListener('click', (event) => {
+      event.preventDefault()
+      const message = '私のオススメpastakヘアカラーはこちら！皆さんもオススメのヘアカラーをシェアしませんか？'
+      const thisUrl = `https://haircolor.pastak.net/?hue=${hueRotateContoller.value}&brightness=${brightnessController.value}`
+      const intentUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}&url=${encodeURIComponent(thisUrl)}&hashtags=pastak_haircolor`
+      open(intentUrl)
+    })
+  }
+
+  let searchQueries = {}
   location.search.substr(1).split('&').forEach(t => {
-    const [k, v] = t.split('=');
-    searchQueries[k] = v;
+    const [k, v] = t.split('=')
+    searchQueries[k] = v
   })
   if (Object.keys(searchQueries)) {
-    history.replaceState({}, document.title, '/');
+    history.replaceState({}, document.title, '/')
     if (hueRotateContoller && searchQueries['hue'] !== undefined) {
-      hueRotateContoller.value = searchQueries['hue'];
-      setHueRotate();
+      hueRotateContoller.value = searchQueries['hue']
+      updateFilter()
     }
     if (brightnessController && searchQueries['brightness'] !== undefined) {
-      brightnessController.value = searchQueries['brightness'];
-      setBrightness();
+      brightnessController.value = searchQueries['brightness']
+      updateFilter()
     }
   }
-})();
+})()
