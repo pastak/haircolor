fe78ab490b28299fb58f55b2e19321fdcd32cf67
pastak <pasta0915@gmail.com>
2018-06-19 00:27:33 +0900

    メッシュ対応

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,16 @@
-import browser from 'https://dev.jspm.io/bowser';
+// import browser from 'https://dev.jspm.io/bowser';
 (() => {
   'use strict';
   if (location.protocol === 'http:' && location.hostname !== 'localhost') {
     return location.protocol = 'https:'
   }
-  if (browser.safari) {
-    if (confirm('このウェブサイトはお使いの環境では動作しません。Android Chromeまたはデスクトップ版のFirefoxやChromeなどでの閲覧をお願いします。ところで、この問題を解決するためのPRを作成してみませんか？')){
-      location.href = 'https://github.com/pastak/haircolor'
-    }
-  }
+  // if (browser && browser.safari) {
+  //   if (confirm('このウェブサイトはお使いの環境では動作しません。Android Chromeまたはデスクトップ版のFirefoxやChromeなどでの閲覧をお願いします。ところで、この問題を解決するためのPRを作成してみませんか？')){
+  //     location.href = 'https://github.com/pastak/haircolor'
+  //   }
+  // }
 
-  const hairImage = document.querySelector('#hairImage')
+  let hairImage = document.querySelector('#hairImage')
   const faceImage = document.querySelector('#faceImage')
   const canvas = document.querySelector('#resultCanvas')
   const ctx = canvas.getContext('2d')
@@ -27,6 +27,7 @@ import browser from 'https://dev.jspm.io/bowser';
   const hueRotateContoller = document.querySelector('#hueRotateContoller')
   const brightnessController = document.querySelector('#brightnessController')
   const saturateController = document.querySelector('#saturateController')
+
   const updateFilter = () => {
     const filter = `
       hue-rotate(${hueRotateContoller.value}deg)
@@ -35,6 +36,21 @@ import browser from 'https://dev.jspm.io/bowser';
     `
     updateCanvas(filter)
   }
+
+  const isMeshCheckbox = document.querySelector('#isMeshCheckbox')
+
+  const toggleMeshMode = (state) => {
+    hairImage = state
+      ? document.querySelector('#meshImage')
+      : document.querySelector('#hairImage')
+
+    updateFilter();
+  }
+
+  isMeshCheckbox.addEventListener('change', () => {
+    toggleMeshMode(isMeshCheckbox.checked)
+  });
+
   const eventNames = ['input', 'change']
   eventNames.forEach((eventName) => {
     hueRotateContoller && hueRotateContoller.addEventListener(eventName, updateFilter)
@@ -60,11 +76,11 @@ import browser from 'https://dev.jspm.io/bowser';
 
 
   const tweetButton = document.querySelector('#tweetButton')
-  if (tweetButton && hueRotateContoller && brightnessController && saturateController) {
+  if (tweetButton && hueRotateContoller && brightnessController && saturateController && isMeshCheckbox) {
     tweetButton.addEventListener('click', (event) => {
       event.preventDefault()
       const message = '私のオススメpastakヘアカラーはこちら！皆さんもオススメのヘアカラーをシェアしませんか？'
-      const thisUrl = `https://haircolor.pastak.net/?hue=${hueRotateContoller.value}&brightness=${brightnessController.value}&saturate=${saturateController.value}`
+      const thisUrl = `https://haircolor.pastak.net/?hue=${hueRotateContoller.value}&brightness=${brightnessController.value}&saturate=${saturateController.value}&mesh=${isMeshCheckbox.checked}`
       const intentUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}&url=${encodeURIComponent(thisUrl)}&hashtags=pastak_haircolor`
       open(intentUrl)
     })
@@ -88,6 +104,11 @@ import browser from 'https://dev.jspm.io/bowser';
     if (saturateController && searchQueries['saturate'] !== undefined) {
       saturateController.value = searchQueries['saturate']
       updateFilter()
+    }
+    if (isMeshCheckbox && searchQueries['mesh'] !== undefined) {
+      const isMesh = !!JSON.parse(searchQueries['mesh'])
+      isMeshCheckbox.checked = isMesh
+      toggleMeshMode(isMesh)
     }
   }
 })()
